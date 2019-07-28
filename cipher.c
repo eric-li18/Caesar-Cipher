ba07234f0b9dc87a4a39188b785889bb821c6f24
Eric <erickh.li@mail.utoronto.ca>
2019-07-28 10:33:57 -0400

    added input

--- cipher.c
+++ cipher.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #define MAX_STR_LEN 2048
 
-void caesar_cipher(unsigned char plaintext[MAX_STR_LEN], int displacement)
-{
+void caesar_cipher(unsigned char plaintext[MAX_STR_LEN], int displacement){
+
     int i=0;
     while(plaintext[i]!='\0'){
         if(((int)plaintext[i] + displacement) != 0)
@@ -12,8 +13,7 @@ void caesar_cipher(unsigned char plaintext[MAX_STR_LEN], int displacement)
     }
 }
 
-void caesar_decipher(unsigned char cryptotext[MAX_STR_LEN],int displacement)
-{    
+void caesar_decipher(unsigned char cryptotext[MAX_STR_LEN],int displacement){
     int i=0;
     while(cryptotext[i]!='\0'){
         if(((int)cryptotext[i] - displacement) != 0)
@@ -22,6 +22,24 @@ void caesar_decipher(unsigned char cryptotext[MAX_STR_LEN],int displacement)
     }
 }
 
-int main(){
+int main()
+{
+    unsigned char str[MAX_STR_LEN];
+
+    while(1){
+        printf("Enter text to be decoded or 'exit' to exit the program:\n");
+        fgets((char* )str, MAX_STR_LEN,stdin);
+        if(strcmp((char *)str,"exit\n") == 0){
+            return 0;
+        }
+        printf("Original text: %s\n",str);
+        int shift = rand()%256+1;
+        caesar_cipher(str,shift);
+        printf("Encrypted text: %s\n",str);
+        caesar_decipher(str,shift);
+        printf("Decrypted text: %s\n",str);
+    }
+   
     return 0;
+    
 }
